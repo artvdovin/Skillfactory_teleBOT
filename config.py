e4da5a1507a2af102b75d90950a37e23de198bb9
artvdovin <130549394+artvdovin@users.noreply.github.com>
2024-01-06 11:20:46 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = '6315915088:AAFL8WyjY6Npx5qTUwMwuuTi_94FtATcb6U'
+APIKEY ='e9367905cd6956680a45212691740155'
+
+keys= {
+    'доллар':'USD',
+    'евро': 'EUR',
+    'рубль':'RUB'
+}
---
+++ extensions.py
@@ -0,0 +1,32 @@
+from config import APIKEY,keys
+
+import requests
+
+class ConvertionException(Exception):
+    pass
+
+class Convertion:
+    @staticmethod
+    def convert(quote:str, base:str, amount:str):
+
+
+        if quote == base:
+            raise ConvertionException(f'Невозможно перевести одинаковые валюты:{base}')
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту {quote}')
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту {base}')
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConvertionException(f'Не удалось обработать количество {amount}')
+        r = requests.get(f'http://apilayer.net/api/live?access_key={APIKEY}&currencies={quote_ticker}&source={base_ticker}&format=1')
+        key = base_ticker+quote_ticker
+        total_base = str(r.json()['quotes'][key])
+
+
+        return total_base
